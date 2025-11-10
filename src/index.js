e26388743884afdb3f10cc8f4c9f90542d23a565
4424904 <4424904@gmail.com>
2025-11-10 17:10:07 +0900

    [20251110] 커서 업데이트 롤백

--- src/index.js
+++ src/index.js
@@ -3,27 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import mouseCursor from './assets/icons/mouse.png'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+);
 
-// Dinory 전용 커서 설정
-document.addEventListener("DOMContentLoaded", () => {
-  const cursorUrl = `url(${mouseCursor}) 4 4, auto`;
-  document.body.style.cursor = cursorUrl;
-
-  // hover 시 커서 유지
-  const hoverTargets = document.querySelectorAll("a, button, input, textarea");
-  hoverTargets.forEach((el) => {
-    el.addEventListener("mouseenter", () => {
-      el.style.cursor = `url(${mouseCursor}) 4 4, pointer`;
-    });
-    el.addEventListener("mouseleave", () => {
-      el.style.cursor = cursorUrl;
-    });
-  });
-
-});
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
